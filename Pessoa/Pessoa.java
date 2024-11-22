ce4be33adeb62fe6cbbfa95faf50e55ba2b70c31
Eduardo Miranda <102879023+sousax@users.noreply.github.com>
2024-11-22 11:18:09 -0300

    Add Files Via Upload

---
+++ Pessoa/Pessoa.java
@@ -0,0 +1,50 @@
+//Aluno: Eduardo Miranda CB3026604
+
+public class Pessoa {
+    private String nome;
+    private char sexo;
+    private int idade;
+    static int kp;
+
+    Pessoa(){
+    kp++;
+    }
+
+    Pessoa(String nome, char sexo, int idade){
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        kp++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    static int getKp(){
+        return kp;
+    }
+
+
+
+
+}
